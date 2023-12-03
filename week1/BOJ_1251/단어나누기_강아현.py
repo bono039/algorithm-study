13c8b4e2c6b7df207c86e4ea1b6ed96a7905d991
ahyun39 <70644095+ahyun39@users.noreply.github.com>
2023-12-03 19:54:50 +0900

    단어나누기 combinations 2로 수정

--- week1/BOJ_1251/단어나누기_강아현.py
+++ week1/BOJ_1251/단어나누기_강아현.py
@@ -5,7 +5,7 @@ idx_list = [i for i in range(len(word))]
 change_word = []
 # 단어의 인덱스를 중복없는 조합으로 선택한 후
 # 인덱스를 기준으로 단어 자름
-for i in combinations(idx_list,3):
+for i in combinations(idx_list,2):
     # divide to three words
     a, b, c = word[:idx_list[i[0]]+1], word[idx_list[i[0]]+1:idx_list[i[1]]+1], word[idx_list[i[1]]+1:]
     # reverse
