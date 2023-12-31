ac55d0693d021e0becbdce1143443655fcfb7f5e
ahyun39 <70644095+ahyun39@users.noreply.github.com>
2023-12-31 16:39:36 +0900

    22858_원상 복구 (small)

--- week4/BOJ_22858/원상복구_강아현.py
+++ week4/BOJ_22858/원상복구_강아현.py
@@ -1,9 +1,9 @@
 n, k = map(int,input().split())
-s = list(map(int,input().split()))
+s = list(map(int,input().split())) # 셔플된 카드
 d = list(map(int,input().split()))
 
 def shuffle(s, n):
-    p = [0 for _ in range(n)]
+    p = [0 for _ in range(n)] # 셔플되기 전 카드 리스트
     for i in range(n):
         p[d[i]-1] = s[i]
     return p
