87249e260f9573363126d59fed1f8ceb276df2c6
bono039 <bono039@hanyang.ac.kr>
2024-01-07 16:30:47 +0900

    [PSG] 택배 배달과 수거하기 / level2 / 60분 / X

--- week5/PSG/택배배달과수거하기_한의정.java
+++ week5/PSG/택배배달과수거하기_한의정.java
@@ -1,2 +1,21 @@
 public class 택배배달과수거하기_한의정 {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+
+        int d = 0;  // 배달해야 하는 택배
+        int p = 0;  // 수거해야 하는 택배
+
+        for(int i = n - 1 ; i >= 0 ; i--) { // 제일 먼 거릴부터 배달과 수거
+            d -= deliveries[i];
+            p -= pickups[i];
+
+            while(d < 0 || p < 0) { // 배달 가능한 택배 용량이 초과된 경우, 다시 방문
+                d += cap;
+                p += cap;
+                answer += (i + 1) * 2;  // 왕복거리 더하기
+            }
+        }
+
+        return answer;
+    }
 }
