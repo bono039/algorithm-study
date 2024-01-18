6b7bbec0c5757cb2f60037f11b9c088e7d341e13
bono039 <bono039@hanyang.ac.kr>
2024-01-18 12:57:48 +0900

    [BOJ] 17626_Four Squares / 실버3 / 30분 / X

--- week7/BOJ_17626/FourSquares_한의정.java
+++ week7/BOJ_17626/FourSquares_한의정.java
@@ -1,2 +1,24 @@
+import java.util.*;
+import java.io.*;
+
 public class FourSquares_한의정 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+        // dp 배열 초기화
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;  // dp[0]이 꼭 0이어야 함
+        dp[1] = 1;
+
+        for(int i = 2 ; i <= N ; i++) {
+            for(int j = 1 ; j * j <= i ; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
 }
