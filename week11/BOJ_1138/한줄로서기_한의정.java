931cf781e291c99a49fa57f682c9beb9a321acc3
bono039 <bono039@hanyang.ac.kr>
2024-02-28 16:15:35 +0900

    [BOJ] 1138_한 줄로 서기 / 실버2 / 35분 / X

--- week11/BOJ_1138/한줄로서기_한의정.java
+++ week11/BOJ_1138/한줄로서기_한의정.java
@@ -1,2 +1,30 @@
+import java.util.*;
+import java.io.*;
+
 public class 한줄로서기_한의정 {
+    static int N;
+    static int[] arr;
+    static List<Integer> ans = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 1 ; i <= N ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 키가 큰 순으로 입력받은 키대로 List에 삽입하기
+        for(int i = N ; i >= 1 ; i--) {
+            ans.add(arr[i], i); // arr[i]번째에 i 추가
+        }
+
+        for(int k : ans) {
+            System.out.print(k + " ");
+        }
+    }
 }
