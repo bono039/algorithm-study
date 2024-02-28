5aacd852c772896f53e1411717ea4f232d4bf8e2
bono039 <bono039@hanyang.ac.kr>
2024-02-28 14:24:56 +0900

    [BOJ] 1205_등수 구하기 / 실버4 / 120분 / X

--- week11/BOJ_1205/등수구하기_한의정.java
+++ week11/BOJ_1205/등수구하기_한의정.java
@@ -1,2 +1,48 @@
+import java.util.*;
+import java.io.*;
+
 public class 등수구하기_한의정 {
+    static int N, tsScore, P;
+    static int[] A;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        tsScore = Integer.parseInt(st.nextToken());
+        P = Integer.parseInt(st.nextToken());
+
+        if(N == 0) {
+            System.out.println(1);
+            return;
+        }
+
+        A = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < N ; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+
+        if(N == P && tsScore <= A[A.length - 1]) {  // 리스트가 꽉 찼고, 태수의 새로운 점수가 최하위 점수보다 작거나 같다면 못 끼므로 -1 출력
+            answer = -1;
+        }
+        else {  // 그게 아니라면, 랭킹 구하기
+            int rank = 1;
+
+            for(int i = 0 ; i < N ; i++) {
+                if(tsScore < A[i])
+                    rank++;
+                else
+                    break;
+            }
+
+            answer = rank;
+        }
+
+        System.out.println(answer);
+    }
 }
